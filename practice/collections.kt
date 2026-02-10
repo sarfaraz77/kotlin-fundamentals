ead9fb0de6a29a04584aa484e93be40d5727ff39
Sarfaraz Shaikh <sarfaraz.encode@gmail.com>
2026-02-10 12:51:28 +0530

    Add collections file

---
+++ practice/collections.kt
@@ -0,0 +1,41 @@
+data class Event(
+	val title: String,
+    val description: String? = null,
+    val daypart: Daypart,
+    val durationInMinutes: Int,
+)
+
+enum class Daypart {
+    MORNING,
+    AFTERNOON,
+    EVENING
+}
+
+ val Event.durationOfEvent: String
+		get() = if (this.durationInMinutes < 60) {
+           	 "short"
+		} else {
+           	 "long"
+}
+fun main() {
+	val event1 = Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0)
+    val event2 = Event(title = "Eat breakfast", daypart = Daypart.MORNING, durationInMinutes = 15)
+    val event3 = Event(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, durationInMinutes = 60)
+    val event4 = Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60)
+    val event5 = Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10)
+    val event6 = Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
+    
+    val events = mutableListOf<Event>(event1,event2,event3,event4,event5,event6)
+    
+    val shortEvents = events.filter { it.durationInMinutes < 60 }
+    println("You have ${shortEvents.size} short events.")
+
+    val eventsSummary = events.groupBy { it.daypart }
+    eventsSummary.forEach {(daypart, scopeEvents) ->
+    	println("$daypart: ${scopeEvents.size} events")
+    }
+    
+    println("Last event of the day: ${events.last().title}\n")
+    println("Duration of first event of the day: ${events[0].durationOfEvent}")
+
+}
